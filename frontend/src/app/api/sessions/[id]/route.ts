5cc4eae7f8282e8d31af689b77a33e533d0ffe7c
Bi Rong Liu <62890027+birongliu@users.noreply.github.com>
2026-04-27 23:36:56 -0400

    Revert "fix some issue"

--- frontend/src/app/api/sessions/[id]/route.ts
+++ frontend/src/app/api/sessions/[id]/route.ts
@@ -1,6 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
 import { NextRequest, NextResponse } from "next/server";
-import type { Database } from "@/lib/supabase/database.types";
 
 export const runtime = "edge";
 
@@ -25,7 +24,7 @@ export async function GET(req: NextRequest, { params }: GetSessionParams) {
   );
   const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY ?? "";
 
-  const supabase = createClient<Database>(supabaseUrl, supabaseKey, {
+  const supabase = createClient(supabaseUrl, supabaseKey, {
     global: { headers: { Authorization: `Bearer ${token}` } },
     auth: { persistSession: false, autoRefreshToken: false },
   });
@@ -100,7 +99,7 @@ export async function PATCH(req: NextRequest, { params }: GetSessionParams) {
   );
   const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY ?? "";
 
-  const supabase = createClient<Database>(supabaseUrl, supabaseKey, {
+  const supabase = createClient(supabaseUrl, supabaseKey, {
     global: { headers: { Authorization: `Bearer ${token}` } },
     auth: { persistSession: false, autoRefreshToken: false },
   });
