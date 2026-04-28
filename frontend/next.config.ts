3f3c535b5ce55a0c002932b6009b5be7bb161d9d
birongliu <62890027+birongliu@users.noreply.github.com>
2026-04-27 21:55:44 -0400

    fix: remove module type declaration from package.json and add turbopack config in next.config.ts

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  turbopack: {},
   webpack: (config, { webpack, isServer }) => {
     config.experiments = {
       ...config.experiments,
